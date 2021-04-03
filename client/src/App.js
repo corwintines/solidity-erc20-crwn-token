1dde2598c6b3892fe7e6cc72bc8f875398f7b9f2
Corwin Smith <corwinsmith@Corwins-MBP.hitronhub.home>
2021-04-03 11:07:45 -0600

    feat: create UI

---
+++ client/src/App.js
@@ -0,0 +1,69 @@
+// Libraries
+import React, { Component } from "react";
+
+// Contracts
+import Token from './build/contracts/Token.json'
+
+// Styles
+import "./App.css";
+
+// Utils
+import getWeb3 from './getWeb3'
+
+class App extends Component {
+  state = {
+    web3: null,
+    account: null,
+    contract: null,
+    balance: null,
+    sendAmount: '',
+    sendAddress: '',
+  }
+
+  componentDidMount = async () => {
+    // Get web3 instance
+    const web3 = await getWeb3();
+    
+    // Use web3 to get access to users account
+    const account = await web3.eth.getAccounts();
+
+    // Get Token contract
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = Token.networks[networkId];
+    const contract = new web3.eth.Contract(
+      Token.abi,
+      deployedNetwork && deployedNetwork.address,
+    );
+
+    this.setState({web3, account: account[0], contract}, this.getUserBalance)
+  };
+
+  getUserBalance = async () => {
+    // Get balance in wei
+    const balanceInWei = await this.state.contract.methods.balanceOf(this.state.account).call()
+    const balance = await this.state.web3.utils.fromWei(balanceInWei)
+    this.setState({balance})
+  }
+
+  sendTransaction = async () => {
+    const weiAmount = await this.state.web3.utils.toWei(this.state.sendAmount)
+    await this.state.contract.methods.transfer(this.state.sendAddress, weiAmount).send({from: this.state.account})
+    this.getUserBalance()
+  }
+
+  render() {
+    return (
+      <div className="Container">
+        <div className="App">
+          <h1>CRWN Trade</h1>
+          <p>Balance: {this.state.balance} CRWN</p>
+          <input type="number" onChange={(e) => this.setState({sendAmount: e.target.value})} />
+          <input onChange={(e) => this.setState({sendAddress: e.target.value})} />
+          <button onClick={this.sendTransaction}>send</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
